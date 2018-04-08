c2419220d4e0c34ae8c7e0a83694169e59f6df93
MorganMartin12 <MorganMartin12@github>
2018-04-07 21:04:38 -0400

    gay

--- prog4/prog4/msh.cpp
+++ prog4/prog4/msh.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-
+#include <string>
 using namespace std; 
 
-void loop() {
+string read() {
 
 }
+string* tokenizer(string input) {
+
+}
+int functions(string* tokens) {
+
+}
+
+void loop() {
+	string input; 
+	string* tokens;
+	int status = 1;
+	while (status) {
+		input = read();
+		tokens = tokenizer(input);
+		status = functions(tokens);
+		delete tokens; 
+	}
+}
+
 int main(int argc, char **argv) {
 	loop();
 	return 0; 
